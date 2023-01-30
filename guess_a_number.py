ad6cea8c09d034c26812b5ae7ea5521545bf7668
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-01-30 17:05:57 +0200

    Update to guess_a_number_file
    Added new game conditions

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,19 +1,132 @@
 import random
 computer_number = random.randint(1, 100)
+remaining_attempts = 20
+level = 1
+player_input = ""
+no_attempts_remaining = False
+game_won = False
+
+print(f"Hi! Guess the computer number. Level: {level}. You can make {remaining_attempts} attempts.")
 
 while True:
-    player_input = input("Guess the number (1-100): ")
 
-    if not player_input.isdigit():
-        print("Invalid input! Try again...")
-        continue
+    while level == 1:
+        player_input = input("Guess the number (1-100): ")
 
-    player_number = int(player_input)
+        if not player_input.isdigit():
+            print("Invalid input! Try again...")
+            continue
 
-    if player_number == computer_number:
-        print("You guessed it!")
+        player_number = int(player_input)
+
+        if player_number > computer_number:
+            remaining_attempts -= 1
+            print("Too high!")
+
+            if remaining_attempts == 0:
+                no_attempts_remaining = True
+                print(f"No more attempts!")
+                break
+            else:
+                print(f"Remaining attempts: {remaining_attempts}")
+
+        elif player_number < computer_number:
+            remaining_attempts -= 1
+            print("Too low!")
+
+            if remaining_attempts == 0:
+                no_attempts_remaining = True
+                print(f"No more attempts!")
+                break
+            else:
+                print(f"Remaining attempts: {remaining_attempts}")
+
+        else:
+            print("You guessed it!")
+            level += 1
+            computer_number = random.randint(1, 200)
+            remaining_attempts = 15
+            print(f"Level: {level}. You can make {remaining_attempts} attempts.")
+
+        while level == 2:
+            player_input = input("Guess the number (1-200): ")
+
+            if not player_input.isdigit():
+                print("Invalid input! Try again...")
+                continue
+
+            player_number = int(player_input)
+
+            if player_number > computer_number:
+                remaining_attempts -= 1
+                print("Too high!")
+
+                if remaining_attempts == 0:
+                    no_attempts_remaining = True
+                    print(f"No more attempts!")
+                    break
+                else:
+                    print(f"Remaining attempts: {remaining_attempts}")
+
+            elif player_number < computer_number:
+                remaining_attempts -= 1
+                print("Too low!")
+
+                if remaining_attempts == 0:
+                    no_attempts_remaining = True
+                    print(f"No more attempts!")
+                    break
+                else:
+                    print(f" Remaining attempts: {remaining_attempts}")
+
+            else:
+                print("You guessed it!")
+                level += 1
+                computer_number = random.randint(1, 300)
+                remaining_attempts = 10
+                print(f"Level: {level}. You can make {remaining_attempts} attempts.")
+
+            while level == 3:
+                player_input = input("Guess the number (1-300): ")
+
+                if not player_input.isdigit():
+                    print("Invalid input! Try again...")
+                    continue
+
+                player_number = int(player_input)
+
+                print(f"You have {remaining_attempts} attempts.")
+
+                if player_number > computer_number:
+                    remaining_attempts -= 1
+                    print("Too high!")
+
+                    if remaining_attempts == 0:
+                        no_attempts_remaining = True
+                        print(f"No more attempts!")
+                        break
+                    else:
+                        print(f" Remaining attempts: {remaining_attempts}")
+
+                elif player_number < computer_number:
+                    remaining_attempts -= 1
+                    print("Too low!")
+
+                    if remaining_attempts == 0:
+                        no_attempts_remaining = True
+                        print(f"No more attempts!")
+                        break
+                    else:
+                        print(f" Remaining attempts: {remaining_attempts}")
+
+                else:
+                    print("You guessed it!")
+                    game_won = True
+                    break
+
+    if game_won:
+        print("Congrats, you win!")
         break
-    elif player_number > computer_number:
-        print("Too high!")
-    else:
-        print("Too low!")
+    if no_attempts_remaining:
+        print("Sorry, you lost!")
+        break
